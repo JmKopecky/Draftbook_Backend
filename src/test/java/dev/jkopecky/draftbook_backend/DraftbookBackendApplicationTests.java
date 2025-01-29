e4e574a0a0eb79e8c5e2cf7f3b6b887023614b73
Joseph Kopecky <joseph.m.kopecky@gmail.com>
2025-01-28 19:47:11 -0600

    Initial commit.

---
+++ src/test/java/dev/jkopecky/draftbook_backend/DraftbookBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.jkopecky.draftbook_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DraftbookBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
